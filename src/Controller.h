5ffc7dc648af56973690a516d6675fe2671388bf
morath <schumov.nn@gmail.com>
2024-03-21 16:37:07 +0500

    Added comments

--- src/Controller.h
+++ src/Controller.h
@@ -2,8 +2,12 @@
 
 #include "Params.h"
 
+/// Основной класс контроллера спутника.
 class Controller {
 public:
   Controller();
+
+  /// Рассчитывает следующее действие спутника на основе переданных данных о
+  /// текущем состоянии. Вызывается на каждом шагу.
   StepResult calculate_step(StepParams params);
 };
